b86d2f43536873bf17111d4935c1b65e1dfc4f07
NightUnivers <yanlucascarvalho18@gmail.com>
2022-12-08 20:17:59 -0300

    resolvido

---
+++ atividade2/scripts.js
@@ -0,0 +1,27 @@
+function gerar(){
+    let frutas = ["Maça", "Banana", "Uva", "Abacaxi"];
+    let lista = document.getElementById('lista');
+    for(var i = 0; i < frutas.length; i++){
+        let item = document.createElement('li');
+        item.appendChild(document.createTextNode(frutas[i]));
+        lista.appendChild(item);
+    }
+}
+
+var visibilidade = true; 
+function exibir() {
+    document.getElementById("quadrados").style.display = "block";
+}
+function ocultar() {
+    document.getElementById("quadrados").style.display = "none";
+}
+function ocultarExibir() { 
+
+    if (visibilidade) {
+        document.getElementById("quadrados").style.display = "none";
+        visibilidade = false;
+    } else {
+        document.getElementById("quadrados").style.display = "block";
+        visibilidade = true;
+    }
+}
